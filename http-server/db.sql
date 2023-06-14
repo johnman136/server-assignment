84fde42757a12e64c6dd3c8faa9eb8f99e553c61
johnman136 <136196734+johnman136@users.noreply.github.com>
2023-06-14 15:40:55 +0800

    Update db.sql

--- http-server/db.sql
+++ http-server/db.sql
@@ -1,8 +1,13 @@
 use testdb;
-delete from data;
-alter table data auto_increment = 1;
-#insert into data(sender,receiver,message) values(1,1,1);
-#SELECT message from data WHERE (receiver = "b" AND sender = "a") OR (sender = "a" AND receiver = "b");
+
+CREATE TABLE data(
+	ID INT PRIMARY KEY AUTO_INCREMENT,
+    sender VARCHAR(100),
+    receiver VARCHAR(100),
+    message VARCHAR(10000),
+    time_stamp INT
+);
+
 select * from data
 
     
