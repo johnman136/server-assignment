2dd4983d616d6f70b766e4ae3cce0030e3e981fa
johnman136 <136196734+johnman136@users.noreply.github.com>
2023-06-14 15:45:01 +0800

    Update db.sql

--- http-server/db.sql
+++ http-server/db.sql
@@ -1,7 +1,7 @@
 use testdb;
 
 CREATE TABLE data(
-	ID INT PRIMARY KEY AUTO_INCREMENT,
+    ID INT PRIMARY KEY AUTO_INCREMENT,
     sender VARCHAR(100),
     receiver VARCHAR(100),
     message VARCHAR(10000),
